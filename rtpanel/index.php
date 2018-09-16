8309de1fdffeba73fa6f0e18f0b0f5a408c89329
dev-nishant <43321519+dev-nishant@users.noreply.github.com>
2018-09-17 00:00:19 +0530

    Add files via upload
    Folder upload

---
+++ rtpanel/index.php
@@ -0,0 +1,219 @@
+<?php include('header.php');?>
+
+<section class="banner">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-lg-8 col-xl-8 col-marg">
+				<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+					    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+					    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+					    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+					    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+					    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+					</ol>
+					<div class="carousel-inner">
+					    <div class="carousel-item active">
+					      <img class="d-block w-100 img-fluid" src="images/banner.png" alt="First slide">
+					    </div>
+					    <div class="carousel-item">
+					      <img class="d-block w-100 img-fluid" src="images/banner.png" alt="Second slide">
+					    </div>
+					    <div class="carousel-item">
+					      <img class="d-block w-100 img-fluid" src="images/banner.png" alt="Third slide">
+					    </div>
+					    <div class="carousel-item">
+					      <img class="d-block w-100 img-fluid" src="images/banner.png" alt="Fourth slide">
+					    </div>
+					    <div class="carousel-item">
+					      <img class="d-block w-100 img-fluid" src="images/banner.png" alt="Fifth slide">
+					    </div>
+					</div>
+					<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+					    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					    <span class="sr-only">Previous</span>
+					  </a>
+					<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+					    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+					    <span class="sr-only">Next</span>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-12 col-lg-4 col-xl-4 col-panel">
+				<div class="wel-col">
+				<h4 class="welcome-color">Welcome to rtPanel</h4>
+				<div class="panel-cont">
+					<img src="images/wellcome-picture.png" alt="Banner panel" class="img-fluid">
+					<p>Vel adipiscing lectus lundium. Dapibus amet turpis adipiscing ridiculus porta, aenean tempor phasellus cras ultrices urna, ut in turpis auctor, dignissim odio porttitor mus egestas dapibus diam urna phasellus! Amet adipiscing enim, odio odio? Massa eros, ut pulvinar magnis dis, penatibus urna est penatibus turpis egestas non pulvinar, ac lectus sit, habitasse etiam nisi sed habitasse, risus nisi, amet etiam? Pulvinar dis ac vel!</p>
+
+					<a href="#" class="welcome-color">Read More...</a>
+				</div>
+				</div>
+			</div>
+		</div>
+		<hr>
+	</div>
+</section>
+
+<section class="video-slide">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-sm-10 col-md-10 col-lg-10 col-xl-10 offset-sm-1 offset-md-1 offset-lg-1 offset-xl-1">
+				<div class="owl-carousel text-center" id="video">
+				  	<div> <img src="images/video-slider/video1.png" class="img-fluid" alt="Video 1"> </div>
+				  	<div> <img src="images/video-slider/video2.png" class="img-fluid" alt="Video 2"> </div>
+				  	<div> <img src="images/video-slider/video3.png" class="img-fluid" alt="Video 3"> </div>
+				  	<div> <img src="images/video-slider/video4.png" class="img-fluid" alt="Video 4"> </div>
+				  	<div> <img src="images/video-slider/video5.png" class="img-fluid" alt="Video 5"> </div>
+				</div>
+			</div>
+		</div>
+		<hr>
+	</div>
+</section>
+
+<section class="glimpses">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8 col-border">
+				<h5>Glipmses of Exhibition</h5>
+				<div class="row">
+					<div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+						<img src="images/glimpses/golf.png" class="img-fluid" alt="Golf Pic">
+						<p><a href="#">Golf</a></p>
+					</div>
+					<div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+						<img src="images/glimpses/basketball.png" class="img-fluid" alt="Basketball Pic">
+						<p><a href="#">Basketball</a></p>
+					</div>
+					<div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+						<img src="images/glimpses/tennis.png" class="img-fluid" alt="Tennis Pic">
+						<p><a href="#">Tennis</a></p>
+					</div>
+					<div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+						<img src="images/glimpses/football.png" class="img-fluid" alt="Football Pic">
+						<p><a href="#">Football</a></p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+						<img src="images/glimpses/football.png" class="img-fluid" alt="Football Pic">
+						<p><a href="#">Football</a></p>
+					</div>
+					<div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+						<img src="images/glimpses/tennis.png" class="img-fluid" alt="Tennis Pic">
+						<p><a href="#">Tennis</a></p>
+					</div>
+					<div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+						<img src="images/glimpses/golf.png" class="img-fluid" alt="Golf Pic">
+						<p><a href="#">Golf</a></p>
+					</div>
+					<div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+						<img src="images/glimpses/basketball.png" class="img-fluid" alt="Basketball Pic">
+						<p><a href="#">Basketball</a></p>
+					</div>
+				</div>
+				<hr>
+			</div>
+			<div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-news">
+				<h5>News</h5>
+				<div class="news">
+					<img src="images/news-logo.png" class="img-fluid" alt="News Logo">
+					<p>The exhibition will be organized to be the perfect forum.
+						<br>
+						<span>November 4, 2012.</span>
+					</p>
+				</div>
+				<div class="owl-news owl-carousel" id="news">
+					<ul>
+						<li>
+							<i class="fa fa-angle-right" aria-hidden="true"></i>
+							<a href="#">Website coming soon.</a>
+						</li>
+						<li>
+							<i class="fa fa-angle-right" aria-hidden="true"></i>
+							<a href="#">Best sport photogrpher award.</a>
+						</li>
+						<li>
+							<i class="fa fa-angle-right" aria-hidden="true"></i>
+							<a href="#">Educational worshop and meeting.</a>
+						</li>
+						<li>
+							<i class="fa fa-angle-right" aria-hidden="true"></i>
+							<a href="#">Website coming soon.</a>
+						</li>
+						<li>
+							<i class="fa fa-angle-right" aria-hidden="true"></i>
+							<a href="#">Best sport photogrpher award.</a>
+						</li>
+						<li>
+							<i class="fa fa-angle-right" aria-hidden="true"></i>
+							<a href="#">Educational worshop and meeting.</a>
+						</li>
+					</ul>
+				</div>
+				<p class="more"><a href="#">More News</a></p>
+				<hr>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="lat-foll-weat">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3 col-border">
+				<h5>Latest Tweets</h5>
+				<ul>
+					<li>And, we hit about 5,500 Tweets per second (tsp).for context, this TPS is more than Osama Bin Laden's death&on par w/ the japanesse quake.</li>
+					<hr>
+					<li>Within a minite of today's <a href="#">#earthquake,</a>there were more than 40,000 erthquake-related Tweets.</li>
+					<hr>
+					<li>Are Tweets faster than seismic waves? We can't speak to speed of sesmic waves, but a Tweet can reach your followers in less than a second.</li>
+					<hr>
+					<li>Twitter is more than 140 characters. See Twitter in full color. <a href="#">http://t.co/yYnGael</a></li>
+				</ul>
+			</div>
+			<div class="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5 col-border">
+				<h5>Follow us on Facebook</h5>
+			</div>
+			<div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+				<h5>Weather</h5>
+				<img src="images/weather.png" alt="Weather image" class="img-fluid">
+				<hr>
+				<h5>Date and Time</h5>
+				<div id="clockdate">
+				  <div class="clockdate-wrapper">
+				    <div id="date"></div>
+				    <div id="clock"></div>
+				  </div>
+				</div>
+			</div>
+		</div>
+		<hr>
+	</div>
+</section>
+
+<section class="our-partners">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+				<h5>Our Partners</h5>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 col-sm-10 col-md-10 col-lg-10 col-xl-10 offset-sm-1 offset-md-1 offset-lg-1 offset-xl-1 logo-col">
+				<div class="owl-carousel" id="part-logo">
+					<div><img src="images/logo-slider/adidas-logo.png" class="img-fluid" alt="Adidas Logo"/></div>
+					<div><img src="images/logo-slider/cocacola-logo.png" class="img-fluid" alt="Cocacola Logo"/></div>
+					<div><img src="images/logo-slider/sony-logo.png" class="img-fluid" alt="Sony Logo"/></div>
+					<div><img src="images/logo-slider/emirates-logo.png" class="img-fluid" alt="Emirates Logo"/></div>
+					<div><img src="images/logo-slider/visa-logo.png" class="img-fluid" alt="Visa Logo"/></div>
+				</div>
+			</div>
+		</div>
+		<hr>
+	</div>
+</section>
+
+<?php include('footer.php');?>
