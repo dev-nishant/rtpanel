9daf00da3cfa8e4a36b1bc0d5f0e7a63085fbfe7
dev-nishant <43321519+dev-nishant@users.noreply.github.com>
2018-09-16 23:44:13 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!--meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css">
+
+    <!--custom style-->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!--owl carousel-->
+    <link rel="stylesheet" href="assets/OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/OwlCarousel2-2.3.4/dist/assets/owl.theme.default.min.css">
+    
+    <!--font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>rtpanel</title>
+  </head>
+  <body onload="startTime()">
+    <header>
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <ul class="font-12">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Contact Us</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <a href="#"><img src="images/logo.png" class="img-fluid" alt="Logo"></a>
+            <input type="text" class="font-12" placeholder="Google Site Search">
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <section class="nav-header">
+      <div class="container">
+          <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="#"><img src="images/logo.png" alt="Logo"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Participants</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Exhibition Program</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Media</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Travel Guide</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Sponsorships</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Guidelines</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Visitors Corner</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+      </div>
+    </section>
